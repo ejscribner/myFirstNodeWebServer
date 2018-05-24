0acf9d51984c8b0e2226c019f052b4ece6b9947b
ejscribner <ejscribner@gmail.com>
2018-05-23 23:55:15 -0600

    added projects page

--- server.js
+++ server.js
@@ -53,7 +53,13 @@ app.get('/about', (req, res) => {
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
-    });
+    })
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'My Projects',
+    })
 });
 
 app.listen(port, () => {
